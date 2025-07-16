c65dd26f277e6e6499630ec65b48b1f363f9d7f0
wangfugui2025 <huanganyun_1@163.com>
2025-07-16 08:37:11 +0800

    Delete 0326.py

--- 0326.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd  
-data = {'voltage': [3.7, 4.1, 3.5, 4.3], 'temperature': [40, 48, 35, 50]}  
-df = pd.DataFrame(data)  
-df.to_csv('battery_data.csv', index=False)  
-print("异常电池：\n", df[(df['voltage'] > 4.2) | (df['temperature'] > 45)])  
